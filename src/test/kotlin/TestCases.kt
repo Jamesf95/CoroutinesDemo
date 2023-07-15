9d89a7443fb17209d147408da639ceee21290fa2
Jamesf95 <jamesfoot95@gmail.com>
2023-07-15 21:10:49 +0100

    Added tests for all requirements other than the close functionality

--- src/test/kotlin/TestCases.kt
+++ src/test/kotlin/TestCases.kt
@@ -2,6 +2,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.lang.Error
 
 private val mockUser = UserDetails("1", "james")
 private val mockComments = listOf(
@@ -13,7 +14,11 @@ private val mockFriends = listOf(
     UserDetails("5", "Joe Bloggs")
 )
 
+private object MockError: Error()
+
 class TestCases {
+
+    // Loading user
 
     @Test
     fun `When all sources complete successfully, aggregated data is returned`() {
@@ -32,22 +37,126 @@ class TestCases {
         assertEquals(expected, result)
     }
 
+    @Test
+    fun `When loading a mocked user fails, the error is propagated`() {
+        val result = runBlocking {
+            runCatching {
+                LoadAggregatedUserUseCase(
+                    loadUserDetails = { throw MockError },
+                    loadComments = { loadMockComments(it) },
+                    loadFriends = { loadMockFriends(it) }
+                ).loadAggregatedUserDetails()
+            }
+        }
+
+        assertEquals(MockError, result.exceptionOrNull())
+    }
+
+
+    // Loading comments
+
+    @Test
+    fun `When loading comments fails, an empty list is returned`() {
+        val result = runBlocking {
+            runCatching {
+                LoadAggregatedUserUseCase(
+                    loadUserDetails = { loadMockUser() },
+                    loadComments = { throw MockError },
+                    loadFriends = { loadMockFriends(it) }
+                ).loadAggregatedUserDetails()
+            }
+        }
+
+        val expected = AggregatedUserDetails(
+            mockUser, listOf(), mockFriends
+        )
+        val actual = result.getOrNull()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `When loading comments times out, an empty list is returned`() {
+        val result = runBlocking {
+            runCatching {
+                LoadAggregatedUserUseCase(
+                    loadUserDetails = { loadMockUser() },
+                    loadComments = { loadMockComments(it, 3000) },
+                    loadFriends = { loadMockFriends(it) }
+                ).loadAggregatedUserDetails()
+            }
+        }
+
+
+        val expected = AggregatedUserDetails(
+            mockUser, listOf(), mockFriends
+        )
+        val actual = result.getOrNull()
+
+        assertEquals(expected, actual)
+    }
+
+
+    // Friends
+
+    @Test
+    fun `When loading friends fails, an empty list is returned`() {
+        val result = runBlocking {
+            runCatching {
+                LoadAggregatedUserUseCase(
+                    loadUserDetails = { loadMockUser() },
+                    loadComments = { loadMockComments(it) },
+                    loadFriends = { throw MockError }
+                ).loadAggregatedUserDetails()
+            }
+        }
+
+        val expected = AggregatedUserDetails(
+            mockUser, listOf(), mockFriends
+        )
+        val actual = result.getOrNull()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `When loading friends times out, an empty list is returned`() {
+        val result = runBlocking {
+            runCatching {
+                LoadAggregatedUserUseCase(
+                    loadUserDetails = { loadMockUser() },
+                    loadComments = { loadMockComments(it) },
+                    loadFriends = { loadMockFriends(it, 3000) }
+                ).loadAggregatedUserDetails()
+            }
+        }
+
+
+        val expected = AggregatedUserDetails(
+            mockUser, listOf(), mockFriends
+        )
+        val actual = result.getOrNull()
+
+        assertEquals(expected, actual)
+    }
+
+
+
 
     // Functions that return mocks
 
-    private suspend fun loadMockUser(): UserDetails {
-        delay(1)
+    private suspend fun loadMockUser(delayTime: Long = 1): UserDetails {
+        delay(delayTime)
         return mockUser
     }
 
-    private suspend fun loadMockComments(userId: String): List<Comment> {
-        delay(1)
+    private suspend fun loadMockComments(userId: String, delayTime: Long = 1): List<Comment> {
+        delay(delayTime)
         return mockComments
     }
 
-    private suspend fun loadMockFriends(userId: String): List<UserDetails> {
-        delay(1)
+    private suspend fun loadMockFriends(userId: String, delayTime: Long = 1): List<UserDetails> {
+        delay(delayTime)
         return mockFriends
     }
-
 }
